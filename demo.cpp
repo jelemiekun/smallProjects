ad5424c9d24c4dfd83f5a8fd81179991c71f4291
jelemiekun - pc <jeremieeden1946@gmail.com>
2024-10-12 16:19:57 +0800

    add/remove/display done

--- demo.cpp
+++ demo.cpp
@@ -3,15 +3,43 @@
 
 
 int main() {
-    Item apple("Apple", 15, 1.99, Item::ItemCategory::Groceries, "Local Supplier");
+    Item apple("Apple", 2, 1.99, Item::ItemCategory::Groceries, "Local Supplier");
     Item::addItem(apple);
     Item::listItems();
-    Item orange("Orange", 15, 1.99, Item::ItemCategory::Groceries, "Local Supplier");
+    std::cout << "\n\n\n-----\n\n\n";
+
+    Item orange("Orange", 3, 1.99, Item::ItemCategory::Groceries, "Local Supplier");
     Item::addItem(orange);
     Item::listItems();
-    std::cout << "-----" << '\n';
+    std::cout << "\n\n\n-----\n\n\n";
+
+    Item banana("Banana", 1, 1.99, Item::ItemCategory::Groceries, "Local Supplier");
+    Item::addItem(banana);
+    Item::listItems();
+    std::cout << "\n\n\n-----\n\n\n";
+
+    Item pineapple("Pineapple", 4, 1.99, Item::ItemCategory::Groceries, "Local Supplier");
+    Item::addItem(pineapple);
+    Item::listItems();
+    std::cout << "\n\n\n-----\n\n\n";
+
+    Item::removeItem(banana.getID());
+    Item::listItems();
+    std::cout << "\n\n\n-----\n\n\n";
+
+    Item red("red", 3, 1.99, Item::ItemCategory::Groceries, "Local Supplier");
+    Item::addItem(red);
+    Item::listItems();
+    std::cout << "\n\n\n-----\n\n\n";
+
+    Item blue("blue", 1, 1.99, Item::ItemCategory::Groceries, "Local Supplier");
+    Item::addItem(blue);
+    Item::listItems();
+    std::cout << "\n\n\n-----\n\n\n";
+
     Item::removeItem(apple.getID());
     Item::listItems();
+    std::cout << "\n\n\n-----\n\n\n";
     
     return EXIT_SUCCESS;
 }
--- item.cpp
+++ item.cpp
@@ -7,11 +7,13 @@ int Item::sItemListSize = std::size(Item::sItemList);
 int Item::IDCounter = 1;
 
 int* Item::findIndexOfItemToRemove(const int &rItemID) {
-    static int indexes[MAXIMUM_ITEM] = {0};
-    for (int i = 0; i < MAXIMUM_ITEM; i++) { // Iterate the loop
-        if (sItemList[i].mItemID == rItemID) { // If item ID of current index is equal to reference item ID
-            for (int j = 0; j < MAXIMUM_ITEM; j++) { // Then, loop again but for the indexes
-                if (indexes[j] == 0) { // If indexes value is -1 (empty), fill with the current outer loop
+    static int indexes[MAXIMUM_ITEM];
+    std::fill(std::begin(indexes), std::end(indexes), -1); // Fill the array index of -1.
+    
+    for (int i = 0; i < MAXIMUM_ITEM; i++) { // Iterate the loop.
+        if (sItemList[i].mItemID == rItemID) { // If item ID of current index is equal to reference item ID.
+            for (int j = 0; j < MAXIMUM_ITEM; j++) { // Then, loop again but for the indexes to find next empty index.
+                if (indexes[j] == -1) { // If indexes value is -1 (empty), fill with the current outer loop.
                     indexes[j] = i;
                     break;
                 }
@@ -22,10 +24,10 @@ int* Item::findIndexOfItemToRemove(const int &rItemID) {
 }
 
 Item::Item() {
-    mItemID = 0;
+    mItemID = -1;
     mItemName = "";
-    mQuantity = 0;
-    mPrice = 0;
+    mQuantity = -1;
+    mPrice = -1;
     mCategory = Item::ItemCategory::Empty;
     mQuantityLevel = Item::QuantityLevel::Empty;
     mSupplierInformation = "";
@@ -44,7 +46,7 @@ void Item::addItem(const Item &rItem) {
     int quantityCounter = 0;
 
     for (int i = 0; i < sItemListSize; i++) {
-        if (sItemList[i].mItemID == 0) {
+        if (sItemList[i].mItemID == -1) {
             sItemList[i] = rItem;
             quantityCounter++;
         }
@@ -55,10 +57,10 @@ void Item::addItem(const Item &rItem) {
 }
 
 void Item::removeSpecificItem(Item &rItem) {
-    rItem.mItemID = 0;
+    rItem.mItemID = -1;
     rItem.mItemName = "";
-    rItem.mQuantity = 0;
-    rItem.mPrice = 0;
+    rItem.mQuantity = -1;
+    rItem.mPrice = -1;
     rItem.mCategory = Item::ItemCategory::Empty;
     rItem.mQuantityLevel = Item::QuantityLevel::Empty;
     rItem.mSupplierInformation = "";
@@ -68,15 +70,15 @@ void Item::removeItem(const int &rItemID) {
     int* indexes = findIndexOfItemToRemove(rItemID);   
 
     for (int i = 0; i < MAXIMUM_ITEM; i++) {
-        if (indexes[i] != 0) {
-            removeSpecificItem(sItemList[i]);
+        if (indexes[i] != -1) { // If current index is not -1 (not empty), remove the details of that index item.
+            removeSpecificItem(sItemList[indexes[i]]);
         }
     }
 
     for (int i = 0; i < MAXIMUM_ITEM; i++) { // Itirate the loop.
-        if (sItemList[i].mItemID == 0) { // If current item ID is 0, find the next non-empty index and replace it.
+        if (sItemList[i].mItemID == -1) { // If current item ID is -1, find the next non-empty index and replace it.
             for (int j = i; j < MAXIMUM_ITEM; j++) { // Itierate the loop again to find the next non-empty index.
-                if (sItemList[j].mItemID != 0) { // If current item ID is NOT 0, it's not empty. Non-empty index found.
+                if (sItemList[j].mItemID != -1) { // If current item ID is NOT -1, it's not empty. Non-empty index found.
                     sItemList[i] = sItemList[j]; // Move the non-empty index to the current empty index.
                     removeSpecificItem(sItemList[j]); // Remove the moved non-empty index.
                     break;
@@ -88,7 +90,7 @@ void Item::removeItem(const int &rItemID) {
 
 void Item::listItems() {
     for (int i = 0; i < sItemListSize; i++) {
-        if (sItemList[i].mItemID != 0 ) {
+        if (sItemList[i].mItemID != -1) {
             std::cout << i + 1 << ". " << sItemList[i].mItemName << " - " << &sItemList[i] << '\n';
         }
     }
