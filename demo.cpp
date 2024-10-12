c8f2811243e7bbdf0c52354e7a078ca5673213e1
jelemiekun - pc <jeremieeden1946@gmail.com>
2024-10-12 16:25:29 +0800

    wtf?

--- demo.cpp
+++ demo.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include "item.h"
 
-
 int main() {
     Item apple("Apple", 2, 1.99, Item::ItemCategory::Groceries, "Local Supplier");
     Item::addItem(apple);
@@ -41,5 +40,6 @@ int main() {
     Item::listItems();
     std::cout << "\n\n\n-----\n\n\n";
     
+
     return EXIT_SUCCESS;
 }
